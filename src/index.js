d303ee886a1faf3abee9ad2b70eb319048f5dc4a
Vangelis <ev.fotiades@yahoo.com>
2023-02-23 11:04:49 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
-import "./style.css";
+import './style.css';
 
 const todos = [
   {
-    description: "mopping",
+    description: 'mopping',
     completed: true,
     index: 3,
   },
   {
-    description: "clean windows",
+    description: 'clean windows',
     completed: true,
     index: 2,
   },
   {
-    description: "laundry",
+    description: 'laundry',
     completed: false,
     index: 1,
   },
 ];
 
 const todoList = () => {
-  const container = document.getElementById("container");
+  const container = document.getElementById('container');
   todos.sort((a, b) => a.index - b.index);
   for (let i = 0; i < todos.length; i += 1) {
-    const task = document.createElement("div");
-    task.classList.add("todo-el");
+    const task = document.createElement('div');
+    task.classList.add('todo-el');
     task.innerHTML = `
-    <input type="checkbox">
+    <input type='checkbox'>
     <p>${todos[i].description}</p>
     `;
     container.appendChild(task);
   }
 
-  container.addEventListener("change", (event) => {
-    if (event.target.type === "checkbox") {
-      const task = event.target.closest(".todo-el");
-      task.classList.toggle("completed");
+  container.addEventListener('change', (event) => {
+    if (event.target.type === 'checkbox') {
+      const task = event.target.closest('.todo-el');
+      task.classList.toggle('completed');
     }
   });
 };
 todoList();
-
