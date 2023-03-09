7a529998e055577dd2c26c2afbb52fe1f9c5fd79
Vangelis <ev.fotiades@yahoo.com>
2023-03-09 16:20:23 +0200

    Update the add, remove and display function

---
+++ src/__mock__/variables.js
@@ -0,0 +1,5 @@
+export const itemValue = document.getElementById('insertTask');
+export const itemsDisplay = document.getElementById('container');
+export const addItemBtn = document.getElementById('addBtn');
+export const syncBtn = document.getElementById('syncBtn');
+export const eraseAllBtn = document.querySelector('.eraser');
