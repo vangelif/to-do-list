801c3e842ed4c3d5a336ac0c423dfcaa186a0883
Vangelis <ev.fotiades@yahoo.com>
2023-03-10 14:30:13 +0200

    Include delete function

---
+++ src/modules/deleteFunction.js
@@ -0,0 +1,9 @@
+const deleteFunction = (todos, index) => {
+  const removeTasks = todos.filter((element) => element.index !== index + 1);
+  removeTasks.forEach((element, i) => {
+    element.index = i + 1;
+  });
+  return removeTasks;
+};
+
+export default deleteFunction;
